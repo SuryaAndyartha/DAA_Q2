b63ae2e579ab45c31c01b47a8bfa4f9707cda8d3
wdwdngrh <wdnugraha24@gmail.com>
2026-05-15 14:26:35 +0700

    Adjust courier speed and implement return delay
    Increased speed of the courier and added return delay functionality.

--- entities/courier.py
+++ entities/courier.py
@@ -7,7 +7,8 @@ from config.map_data import FLAVOURS
 
 
 class Courier:
-    SPEED = 10
+    SPEED = 20 
+    RETURN_DELAY = 0.5  # in second
 
     def __init__(self):
         self.path = []
@@ -17,8 +18,10 @@ class Courier:
         self.target_px = None
         self.flavour = None
         self.progress = 0.0
+        self.returning = False
+        self.return_timer = 0.0
 
-    def dispatch(self, path, flavour):
+    def dispatch(self, path, flavour): # from shop to customer
         if len(path) < 2:
             return
 
@@ -27,20 +30,41 @@ class Courier:
         self.active = True
         self.flavour = flavour
         self.progress = 0.0
+        self.returning = False
+        self.return_timer = 0.0
 
-        r, c = path[0]
-
-        self.pixel_pos = [
-            c * CELL + CELL // 2,
-            r * CELL + CELL // 2
-        ]
+    def _start_return(self): # reverse method dispatch 
+        self.path = self.path[::-1]
+        self.step_idx = 0
+        self.progress = 0.0
+        self.returning = True
 
     def update(self, dt):
         if not self.active:
             return
 
+        if self.return_timer > 0:
+            self.return_timer -= dt
+            if self.return_timer <= 0:
+                self.return_timer = 0.0
+                self._start_return()
+            return
+
         if self.step_idx >= len(self.path) - 1:
-            self.active = False
+            if not self.returning:
+                r, c = self.path[-1]
+                self.pixel_pos = [
+                    c * CELL + CELL // 2,
+                    r * CELL + CELL // 2,
+                ]
+                self.return_timer = self.RETURN_DELAY
+            else:
+                r, c = self.path[-1]
+                self.pixel_pos = [
+                    c * CELL + CELL // 2,
+                    r * CELL + CELL // 2,
+                ]
+                self.active = False
             return
 
         self.progress += self.SPEED * dt
@@ -49,20 +73,8 @@ class Courier:
             self.progress = 0.0
             self.step_idx += 1
 
-            if self.step_idx >= len(self.path) - 1:
-                self.active = False
-
-                r, c = self.path[-1]
-
-                self.pixel_pos = [
-                    c * CELL + CELL // 2,
-                    r * CELL + CELL // 2
-                ]
-
-                return
-
         r0, c0 = self.path[self.step_idx]
-        r1, c1 = self.path[self.step_idx + 1]
+        r1, c1 = self.path[self.step_idx + 1] if self.step_idx + 1 < len(self.path) else (r0, c0)
 
         t = self.progress
 
@@ -79,6 +91,9 @@ class Courier:
             self.flavour,
             (255, 255, 255)
         )
+
+        if self.returning:
+            col = tuple(max(0, c - 60) for c in col)
 
         x = int(self.pixel_pos[0])
         y = int(self.pixel_pos[1])
